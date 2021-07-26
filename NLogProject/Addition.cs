150f8ffb9596a85c18bef67f06ce115cdea81c18
r-svp26 <riteshk6341@gmail.com>
2021-07-26 11:59:40 +0530

    [Ritesh] Add: NLog Project Implementation

---
+++ NLogProject/Addition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLogProject
+{
+    class Addition
+
+    {
+        static NLog nlog = new NLog();
+        public int Add(int firstNumber, int secondNumber)
+        {
+            if (firstNumber == 0 || secondNumber == 0)
+            {
+                nlog.LogInfo("Sub method");
+                nlog.LogDebug("Debug Successful");
+                nlog.LogError("First Number or Second Number is zero.");
+                return 0;
+            }
+            int sum = firstNumber + secondNumber;
+            nlog.LogInfo("Sub method");
+            nlog.LogDebug("Debug Successful");
+            nlog.LogWarn("First Number or Second Number is zero.");
+            return sum;
+        }
+    }
+}
---
+++ NLogProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NLogProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to NLog");
+            Addition addition = new Addition();
+            addition.Add(10,20);
+        }
+    }
+}
